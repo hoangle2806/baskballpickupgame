d44c2b1b5d852d2cae0cfd1922de6d2f4da70702
Hoang <hoang@admins-MacBook-Pro.local>
2018-10-12 14:39:15 -0700

    temp

--- server.js
+++ server.js
@@ -39,10 +39,10 @@ app.use('/api/posts', posts)
 if(process.env.NODE_ENV === 'production'){
     //set static folder
     app.use(express.static('client/build'));
+    app.get('*',(req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 //==============================================
 
