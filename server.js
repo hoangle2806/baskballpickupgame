8161b5ebbf74c0bae974c6df3a69188a3ba737f1
Hoang <hoang@admins-MacBook-Pro.local>
2018-10-12 13:20:58 -0700

    production config changes

--- server.js
+++ server.js
@@ -35,14 +35,15 @@ app.use('/api/posts', posts)
 
 //===========For Production Purpose==============
 
-// Server static assests if in production
-if(process.env.NODE_ENV === "production"){
-    // set static folder
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 //==============================================
 
